1c69e58c1e00c130bc4e8e47af97e8de25aa4275
Forcaken <99072619+Forcaken@users.noreply.github.com>
2022-05-05 20:52:47 +0700

    Addedi mplementation of finding_bracket2 func

--- src/LibAnother/Correct_input.c
+++ src/LibAnother/Correct_input.c
@@ -21,3 +21,20 @@ bool finding_bracket(char* UKAZAT)
     }
     return true;
 }
+
+bool finding_bracket2(char* UKAZAT)
+{
+    int flag = 0;
+    while (*UKAZAT != 10) {
+        if (*UKAZAT == ')') {
+            flag = 1;
+            break;
+        }
+        UKAZAT++;
+    }
+    if (flag == 0) {
+        printf("Error at column 14: expected ')'");
+        return false;
+    }
+    return true;
+}
