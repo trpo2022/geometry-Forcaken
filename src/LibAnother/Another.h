7ee1f2ee3e038cfcc851780b4a3c5a95d23f9b7d
Forcaken <99072619+Forcaken@users.noreply.github.com>
2022-05-10 22:22:01 +0700

    Fix declaration of correct function

--- src/LibAnother/Another.h
+++ src/LibAnother/Another.h
@@ -4,5 +4,5 @@
 bool finding_bracket(char* UKAZAT);
 bool finding_bracket2(char* UKAZAT);
 bool finding_comma(char* UKAZAT);
-bool correct(char* Circle, char* UKAZAT, int N);
+bool correct(char* Circle, char* UKAZAT);
 void translation_universal(char* UKAZAT, double* x1);
